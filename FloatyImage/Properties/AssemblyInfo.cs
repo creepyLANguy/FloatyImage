e80290724ec69bebb68bc18b8dc05e17f8d9009c
Altamish Mahomed <gambit318@gmail.com>
2023-10-09 12:12:24 +0200

    Version 0.1 and readme.

--- FloatyImage/Properties/AssemblyInfo.cs
+++ FloatyImage/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FloatyImage")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A small tool for easily displaying reference images over the desktop. For more info: https://github.com/creepyLANguy/FloatyImage/")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FloatyImage")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 Supports pinning, locking the canvas, custom controls, colour picking, and other handy features. 
 
 ![image](https://github.com/creepyLANguy/FloatyImage/assets/28150772/27cda232-570d-4316-b887-15b6000f71ac)
+
+- [View on GitHub](https://github.com/creepyLANguy/FloatyImage/)
